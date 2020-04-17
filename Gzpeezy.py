b4d6a812cb6debdd22701896b10f4a1691649088
Guangze Zu <gzu2018@gmail.com>
2020-04-16 21:45:37 -0500

    interhitance

--- Gzpeezy.py
+++ Gzpeezy.py
@@ -137,36 +137,10 @@ class DefaultAgent(CaptureAgent):
 ###################
 ## Attack Agent  ##
 ###################
-class AttackAgent(CaptureAgent):
+class AttackAgent(DefaultAgent):
   """
   A base class for reflex agents that chooses score-maximizing actions
   """
- 
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
-
-    '''
-    Your initialization code goes here, if you need any.
-    '''
-    self.start = gameState.getAgentPosition(self.index)
 
   def chooseAction(self, gameState):
     """
@@ -183,36 +157,10 @@ class AttackAgent(CaptureAgent):
 ###################
 ## Defend Agent  ##
 ###################
-class DefendAgent(CaptureAgent):
+class DefendAgent(DefaultAgent):
   """
   A base class for reflex agents that chooses score-maximizing actions
   """
- 
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
-
-    '''
-    Your initialization code goes here, if you need any.
-    '''
-    self.start = gameState.getAgentPosition(self.index)
 
   def chooseAction(self, gameState):
     """
